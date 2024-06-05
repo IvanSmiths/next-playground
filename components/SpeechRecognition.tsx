db6b38b287066b88b62162d07915bb3010343d7e
IvanSmiths <info@ivansmiths.com>
2024-06-05 23:05:10 +0200

    add error/end handling

--- components/SpeechRecognition.tsx
+++ components/SpeechRecognition.tsx
@@ -10,12 +10,21 @@ const SpeechRecognitionComponent = () => {
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
         const recognition = new SpeechRecognition();
 
-        recognition.start();
+        recognition.start(console.log('start'));
         recognition.onresult = async function(event) {
             const transcript = event.results[0][0].transcript;
+            if (!transcript) {
+                console.error("No transcript was found.");
+            }
             console.log('transcript', transcript)
             setText(transcript);
         }
+        recognition.end = function(event) {
+            console.error('Error in speech recognition: ', event);
+        };
+        recognition.onerror = function(event) {
+            console.error('Error in speech recognition: ', event);
+        };
     }
 
     return (
